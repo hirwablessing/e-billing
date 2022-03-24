2110beb0dfaecb7773cfe6b4c834bb1551657a31
hirwablessing <hirwablessing7@gmail.com>
2022-03-24 17:10:01 +0200

    temp

--- backend/src/__tests__/intergration/meter.test.ts
+++ backend/src/__tests__/intergration/meter.test.ts
@@ -1,95 +1,87 @@
+// @ts-nocheck
+
 import { request } from "undici";
 import { createTestServer } from "../../utils/test-utils";
 
 const { serverURL } = createTestServer();
 
 const meterStructure = {
-    id: expect.any(Number),
-    number: expect.any(String),
-    days: expect.any(Number),
+  id: expect.any(Number),
+  number: expect.any(String),
+  days: expect.any(Number),
 };
 
 describe("Meter API", () => {
-    describe("GET /api/meters", () => {
-        it("Should return meters", async () => {
-            const { statusCode, body, headers } = await request(
-                `${serverURL}/api/meters`
-            );
+  describe("GET /api/meters", () => {
+    it("Should return meters", async () => {
+      const { statusCode, body, headers } = await request(
+        `${serverURL}/api/meters`
+      );
 
-            const respData = await body.json();
+      const respData = await body.json();
 
-            expect(headers["content-type"]).toMatch(
-                /application\/json/
-            );
+      expect(headers["content-type"]).toMatch(/application\/json/);
 
-            expect(statusCode).toBe(200);
+      expect(statusCode).toBe(200);
 
-            for (const meters of respData) {
-                expect(meters).toMatchObject(meterStructure);
-            }
-        });
+      for (const meters of respData) {
+        expect(meters).toMatchObject(meterStructure);
+      }
+    });
+  });
+
+  describe("GET /api/meters/by-number/:number", () => {
+    it("should return a meter number on success ", async () => {
+      const { statusCode, body, headers } = await request(
+        `${serverURL}/api/meters/by-number/103434`
+      );
+
+      const respData = await body.json();
+
+      expect(headers["content-type"]).toMatch(/application\/json/);
+
+      expect(statusCode).toBe(200);
+      expect(respData).toMatchObject(meterStructure);
     });
 
-    describe("GET /api/meters/by-number/:number", () => {
-        it("should return a meter number on success ", async () => {
-            const { statusCode, body, headers } = await request(
-                `${serverURL}/api/meters/by-number/103434`
-            );
+    it("should fail on an invalid meter number ", async () => {
+      const { statusCode, body, headers } = await request(
+        `${serverURL}/api/meters/by-number/1`
+      );
 
-            const respData = await body.json();
+      const respData = await body.json();
 
-            expect(headers["content-type"]).toMatch(
-                /application\/json/
-            );
+      expect(headers["content-type"]).toMatch(/application\/json/);
 
-            expect(statusCode).toBe(200);
-            expect(respData).toMatchObject(meterStructure)
-        })
+      expect(statusCode).toBe(400);
+      expect(respData.message).toEqual("Invalid meter number");
+    });
 
-        it("should fail on an invalid meter number ", async () => {
-            const { statusCode, body, headers } = await request(
-                `${serverURL}/api/meters/by-number/1`
-            );
+    it("should fail return zero for non existing id ", async () => {
+      const { statusCode, body, headers } = await request(
+        `${serverURL}/api/meters/by-number/134123`
+      );
 
-            const respData = await body.json();
+      const respData = await body.json();
 
-            expect(headers["content-type"]).toMatch(
-                /application\/json/
-            );
+      expect(headers["content-type"]).toMatch(/application\/json/);
 
-            expect(statusCode).toBe(400);
-            expect(respData.message).toEqual("Invalid meter number")
-        })
+      expect(statusCode).toBe(400);
+      expect(respData.message).toEqual("You have 0 days, buy now!");
+    });
 
-        it("should fail return zero for non existing id ", async () => {
-            const { statusCode, body, headers } = await request(
-                `${serverURL}/api/meters/by-number/134123`
-            );
+    it("should run success for an existing meter", async () => {
+      const { statusCode, body, headers } = await request(
+        `${serverURL}/api/meters/by-number/518550`
+      );
 
-            const respData = await body.json();
+      const respData = await body.json();
 
-            expect(headers["content-type"]).toMatch(
-                /application\/json/
-            );
+      expect(headers["content-type"]).toMatch(/application\/json/);
 
-            expect(statusCode).toBe(400);
-            expect(respData.message).toEqual("You have 0 days, buy now!")
-        })
-
-        it("should run success for an existing meter", async () => {
-            const { statusCode, body, headers } = await request(
-                `${serverURL}/api/meters/by-number/518550`
-            );
-
-            const respData = await body.json();
-
-            expect(headers["content-type"]).toMatch(
-                /application\/json/
-            );
-
-            expect(statusCode).toBe(200);
-            expect(respData.number).toEqual("518550")
-            expect(respData.days).toEqual(0)
-        })
-    })
+      expect(statusCode).toBe(200);
+      expect(respData.number).toEqual("518550");
+      expect(respData.days).toEqual(0);
+    });
+  });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,5 +3,5 @@ import app from "./utils/app";
 const server = app();
 
 server.listen(8000, () => {
-	console.log("Server started on port 8000");
+  console.log("Server started on port 8000");
 });
